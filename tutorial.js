f8542fdf60ad99c5f1b812e4ff089c69a2526477
rishikeshparasar <rishikesh.parasar@nagarro.com>
2023-05-04 08:34:28 +0530

    gfdg

---
+++ tutorial.js
@@ -0,0 +1,10 @@
+const sum=(num1,num2)=>num1+num2;
+const pi=3.14;
+class some{
+    constructor(){
+        console.log("object created");
+    }
+}
+module.exports.sum=sum;
+module.exports.pi=pi;
+module.exports.some=some;
